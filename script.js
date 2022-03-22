3ba46c089215c5c8f969c5ca5514e97b72e7a860
luuu-xu <a550763321@gmail.com>
2022-03-22 14:59:22 -0400

    Updated script.js to using class instead of plain constructors.

--- script.js
+++ script.js
@@ -1,55 +1,62 @@
 let myLibrary = [];
 
-function Book(title, author, pages, isRead) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.isRead = isRead
-}
-Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead ? 'read already' : 'not read yet'}`;
-};
-
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-    return myLibrary.length;
+class Book {
+    constructor(title, author, pages, isRead) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.isRead = isRead
+    }
+    info() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead ? 'read already' : 'not read yet'}`;
+    }
+    addBookToLibrary() {
+        myLibrary.push(this);
+        return myLibrary.length;
+    }
 }
 
 // const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 // const nineteenEightyFour = new Book('1984', 'George Orwell', 298, true);
 // const oneHundredYearsOfSolitude = new Book('One Hundred Years of Solitude', 'Gabriel Garcia Marquez', 417, true);
-// addBookToLibrary(theHobbit);
-// addBookToLibrary(nineteenEightyFour);
-// addBookToLibrary(oneHundredYearsOfSolitude);
+// theHobbit.addBookToLibrary();
+// nineteenEightyFour.addBookToLibrary();
+// oneHundredYearsOfSolitude.addBookToLibrary();
 
-const opener = document.getElementById('opener');
-opener.onclick = function() {
-    const lightbox = document.getElementById('lightbox');
+const openerbox = (() => {
+    const opener = () => {
+        const opener = document.getElementById('opener');
+        opener.onclick = function() {
+            const lightbox = document.getElementById('lightbox');
+        
+            const dimmer = document.createElement('div');
+            dimmer.style.width = window.innerWidth + 'px';
+            dimmer.style.height = window.innerHeight + 'px';
+            dimmer.id = 'dimmer';
+            dimmer.onclick = function() {
+                document.body.removeChild(this);
+                lightbox.style.visibility = 'hidden';
+            }
+            document.body.appendChild(dimmer);
+        
+            lightbox.style.visibility = 'visible';
+            lightbox.style.top = window.innerHeight/2 - 302 + 'px';
+            lightbox.style.left = window.innerWidth/2 - 120 + 'px';
+            return false;
+        }        
+    };
+    return {opener};
+})();
 
-    const dimmer = document.createElement('div');
-    dimmer.style.width = window.innerWidth + 'px';
-    dimmer.style.height = window.innerHeight + 'px';
-    dimmer.id = 'dimmer';
-    dimmer.onclick = function() {
-        document.body.removeChild(this);
-        lightbox.style.visibility = 'hidden';
-    }
-    document.body.appendChild(dimmer);
-
-    lightbox.style.visibility = 'visible';
-    lightbox.style.top = window.innerHeight/2 - 302 + 'px';
-    lightbox.style.left = window.innerWidth/2 - 120 + 'px';
-    return false;
-}
+openerbox.opener();
 
 document.getElementById('form').onsubmit = function() {
-// document.getElementById('btn-submit').onclick = function() {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
     const itsRead = document.getElementById('read').checked;
     const book = new Book(title, author, pages, itsRead);
-    addBookToLibrary(book);
+    book.addBookToLibrary();
     const index = title;
     document.getElementById('form').reset();
 
@@ -114,62 +121,3 @@ document.getElementById('form').onsubmit = function() {
     console.log(myLibrary);
     return false;
 }
-
-
-
-// myLibrary.forEach(function(book) {
-//     const cardDiv = document.createElement('div');
-//     cardDiv.className = 'card';
-
-//     const titleDiv = document.createElement('div');
-//     titleDiv.className = 'title';
-//     titleDiv.appendChild(document.createTextNode(book.title));
-
-//     const authorDiv = document.createElement('div');
-//     authorDiv.className = 'author';
-//     authorDiv.appendChild(document.createTextNode(book.author));
-
-//     const pagesDiv = document.createElement('div');
-//     pagesDiv.className = 'pages';
-//     pagesDiv.appendChild(document.createTextNode(`${book.pages} pages`));
-
-//     const readDiv = document.createElement('div');
-//     readDiv.className = 'read';
-//     readDiv.appendChild(document.createTextNode(`${book.isRead ? 'Read' : 'Not Read'}`));
-
-//     cardDiv.append(titleDiv, authorDiv, pagesDiv, readDiv);
-
-//     const container = document.getElementById('container');
-//     container.appendChild(cardDiv);
-// })
-
-// const btnDelete = document.querySelectorAll('.btn-delete');
-// btnDelete.forEach(btn => {btn.addEventListener('click', function() {
-//     const index = btn.id;
-//     const cardDiv = document.getElementById(index);
-//     console.log(index);
-//     const containerDiv = document.getElementById('container');
-//     console.log(containerDiv);
-//     containerDiv.removeChild(cardDiv);
-// })
-// });
-
-// const btnRead = document.querySelectorAll('.btn-read, .btn-unread');
-// console.log(myLibrary);
-// btnRead.forEach(btn => {btn.addEventListener('click', function() {
-//     const index = btn.id;
-//     const findBook = (element) => element.title === index;
-//     const arrayIndex = myLibrary.findIndex(findBook);
-//     if (btn.className === 'btn-read') {
-//         btn.className = 'btn-unread';
-//         btn.innerHTML = 'Not Read';
-//         console.log(myLibrary[arrayIndex]);
-//         myLibrary[arrayIndex].isRead = false;
-//     } else {
-//         btn.className = 'btn-read';
-//         btn.innerHTML = 'Read';
-//         console.log(myLibrary[arrayIndex]);
-//         myLibrary[arrayIndex].isRead = true;
-//     }
-// })
-// })
